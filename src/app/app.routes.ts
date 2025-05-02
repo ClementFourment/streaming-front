a5eadaedea736d9767e69912ae3c8832c2dbfe63
fourment <clemfourment@hotmail.fr>
2025-05-02 12:00:53 +0200

    front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+    
+    { path: 'video/:title', component: VideoPlayerComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: AuthComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '', component: HomeComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+
+
+
+
+
+
+];
